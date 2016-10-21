5aa8cb1e1644584c7e7ad9be1dd3e3cdc8af830c
hhe <hhe@STUPID-PC703.stnet.local>
2016-10-21 16:56:47 +0300

    In-class Exercise 2

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,9 +1,19 @@
 
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
-// ID:
+// ID: 143
 // Finish time:
 
 public class PlanetExplorer {
+	
+	int pos_x = 0;
+	int pos_y = 0;
+	String facing = "N";
+	String position;	
+	String check;
+	public int x;
+	public int y;
+	public String obstacles;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +22,10 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		
+		this.x = x;
+		this.y = y;
+		this.obstacles = obstacles;
 	}
 	
 	public String executeCommand(String command){
@@ -26,6 +40,138 @@ public class PlanetExplorer {
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		
-		return null;
+		
+		
+		for(int i=0;i<command.length();i++){
+			if(command.substring(i,i+1).equals("l") || command.substring(i,i+1).equals("r")){
+				if(command.substring(i,i+1).equals("l")){
+					if(facing.equals("N")){
+						facing = "W";
+					}
+					else if(facing.equals("E")){
+						facing = "N";
+					}
+					else if(facing.equals("S")){
+						facing = "E";
+					}
+					else if(facing.equals("W")){
+						facing = "S";
+					}
+				}
+				else if(command.substring(i,i+1).equals("r")){
+					if(facing.equals("N")){
+						facing = "E";
+					}
+					else if(facing.equals("E")){
+						facing = "S";
+					}
+					else if(facing.equals("S")){
+						facing = "W";
+					}
+					else if(facing.equals("W")){
+						facing = "N";
+					}
+				}	
+			}
+			else{
+				if(command.substring(i,i+1).equals("f")){
+					if(facing.equals("N")){
+						pos_y++;
+						if(pos_y>=y){
+							pos_y -= y;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_y--;
+							continue;
+						}						
+					}
+					else if(facing.equals("E")){
+						pos_x++;
+						if(pos_x>=x){
+							pos_x -= x;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_x--;
+							continue;
+						}
+					}
+					else if(facing.equals("S")){
+						pos_y--;
+						if(pos_y<0){
+							pos_y += y;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_y++;
+							continue;
+						}
+					}
+					else if(facing.equals("W")){
+						pos_x--;
+						if(pos_x<0){
+							pos_x += x;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_x++;
+							continue;
+						}
+					}
+				}
+				else if(command.substring(i,i+1).equals("b")){
+					if(facing.equals("N")){
+						pos_y--;
+						if(pos_y<0){
+							pos_y += y;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_y++;
+							continue;
+						}
+					}
+					else if(facing.equals("E")){
+						pos_x--;
+						if(pos_x<0){
+							pos_x += x;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_x++;
+							continue;
+						}
+					}
+					else if(facing.equals("S")){
+						pos_y++;
+						if(pos_y>=y){
+							pos_y -= y;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_y--;
+							continue;
+						}
+					}
+					else if(facing.equals("W")){
+						pos_x++;
+						if(pos_x>=x){
+							pos_x -= x;
+						}
+						check = "(" + pos_x + "," + pos_y + ")";
+						if(check.equals(obstacles)){
+							pos_x--;
+							continue;
+						}
+					}
+				}
+			}
+				
+		}
+		
+//		check = "(" + pos_x + "," + pos_y + ")";
+		position = "(" + pos_x + "," + pos_y + "," + facing + ")";
+		return position;
 	}
 }
--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,7 +5,63 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test_executeCommand() {
-		fail("Not yet implemented");
+	public void test_f() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(0,2,N)",explorer.executeCommand("ff"));
 	}
+	
+	@Test
+	public void test_b() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(0,0,N)",explorer.executeCommand("fb"));
+	}
+	
+	@Test
+	public void test_l() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(0,0,W)",explorer.executeCommand("l"));
+	}
+	
+	@Test
+	public void test_r() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(0,0,E)",explorer.executeCommand("r"));
+	}
+	
+	@Test
+	public void test_PrimaryCombo() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(1,2,E)",explorer.executeCommand("ffrf"));
+	}
+	
+	@Test
+	public void test_pos_yBelow0() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(0,99,N)",explorer.executeCommand("b"));
+	}
+	
+	@Test
+	public void test_pos_xBelow0() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");	
+		assertEquals("(99,0,W)",explorer.executeCommand("lf"));
+	}
+	
+	@Test
+	public void test_N_obstacles() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(0,1)");	
+		assertEquals("(0,0,N)",explorer.executeCommand("f"));
+	}
+	
+	@Test
+	public void test_E_obstacles() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(1,0)");	
+		assertEquals("(0,0,E)",explorer.executeCommand("rf"));
+	}
+	
+	@Test
+	public void test_S_obstacles() {
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(0,99)");	
+		assertEquals("(0,0,S)",explorer.executeCommand("rrf"));
+	}
+	
 }
